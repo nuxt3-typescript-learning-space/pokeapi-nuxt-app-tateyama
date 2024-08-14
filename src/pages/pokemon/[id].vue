1b6446fd7aa9233ce17986ab33e504654cce6f24
立山賢 <>
2024-08-14 16:27:50 +0900

    :art: improve: ポケモン詳細ページにパラメータのバリデーションを追加

--- src/pages/pokemon/[id].vue
+++ src/pages/pokemon/[id].vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  // idが数値以外の場合は404エラーに遷移
+  validate: async (route) => {
+    const valid = {
+      // TODO: 他のページでも流用したいためmiddlewareで管理
+      isNumber: /^\d+$/,
+    };
+
+    const { params } = route;
+    const paramId = params.id as string; // アサーションで string || string[] を stringにキャスト
+    return valid.isNumber.test(paramId);
+  },
+});
+</script>
 
 <template>
   <h1>
